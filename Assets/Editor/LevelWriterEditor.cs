ae03db5fff010526626b23b2426ccccb98e7cdd0
UnityLV <104660372+UnityLV@users.noreply.github.com>
2022-12-06 18:54:14 +0300

    Stable

---
+++ Assets/Editor/LevelWriterEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelWriter))]
+public class LevelWriterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        LevelWriter myTarget = (LevelWriter)target;
+
+        
+        if (GUILayout.Button("Write"))
+        {
+            myTarget.WriteLvel();
+        }
+    }
+}
